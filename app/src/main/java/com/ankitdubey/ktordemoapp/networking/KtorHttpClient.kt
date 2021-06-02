26808a35e178e69ae2b78f760800c65e79b0eea9
AnkitDubeyBol <ankit.dubey@lji.io>
2021-06-02 19:31:34 +0530

    Implemented custom views for Miza

--- app/src/main/java/com/ankitdubey/ktordemoapp/networking/KtorHttpClient.kt
+++ app/src/main/java/com/ankitdubey/ktordemoapp/networking/KtorHttpClient.kt
@@ -21,20 +21,16 @@ private const val TIME_OUT = 60_000
 // openssl x509 -in my-certificate.crt -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
 
 
-var cert : CertificatePinner = CertificatePinner.Builder()
-    .add(baseURL, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
-    .add(baseURL, "sha256/f0KW/FtqTjs108NpYj42SrGvOB2PpxIVM8nWxjPqJGE=")
-    .add(baseURL, "sha256/NqvDJlas/GRcYbcWE8S/IceH9cq77kg0jVhZeAPXq8k=")
-    .add(baseURL, "sha256/9+ze1cZgR9KO1kZrVDxA4HQ6voHRCSVNz4RdTCx4U8U=")
-    .add(baseURL, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
-    .build()
+/*var cert : CertificatePinner = CertificatePinner.Builder()
+    .add(baseURL, "Lac=")
+    .build()*/
 
 val ktorHttpClient = HttpClient(OkHttp) {
 
     engine {
-        this.config {
+        /*this.config {
             certificatePinner(cert)
-        }
+        }*/
     }
 
     install(JsonFeature) {
